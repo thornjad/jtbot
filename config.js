310d123864e70afb1f5f806df52d10473462cc24
Jade Thornton <jade@jmthornton.net>
2017-01-31 10:16:26 -0600

    move project to a more flat structure

---
+++ config.js
@@ -0,0 +1,15 @@
+// Uncomment for when deploying to Heroku
+
+// module.exports = {
+//   consumer_key: process.env.CONSUMER_KEY,
+//   consumer_secret: process.env.CONSUMER_SECRET,
+//   access_token: process.env.ACCESS_TOKEN,
+//   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+// };
+
+module.exports = {
+  consumer_key: '0U7ud0Uinj3TW4KhjapkhiTJg',
+  consumer_secret: 'cO9BWwWVU2cD5oUIco3zUIxzrQxxK6gHR6khjEh7YeVdIUWAcD',
+  access_token: '782607461559726081-v1mOXhZVFg7CwMNrXixz0ydiVuOEce9',
+  access_token_secret: 'VyjpOr8Vbpw9xuHqACl2UTrv5sGj61jY146ULD5cl54UH',
+};
