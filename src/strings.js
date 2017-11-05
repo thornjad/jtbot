35ec6d693c2d0f7193114289c099a4b28db8c516
Jade Thornton <jade@jmthornton.net>
2017-11-05 11:54:56 -0600

    improve search string

--- src/strings.js
+++ src/strings.js
@@ -1,13 +1,30 @@
 module.exports = {
     queryString: [
-        'privacy matters',
+        'computer security is important',
+        'javascript',
+        'ecmascript',
+        'ecmascript 2018',
+        'ecmascript 2017',
+        'ecmascript 2016',
+        'ecmascript 2015',
+        'ecmascript2017',
+        'ecmascript2016',
+        'es2015',
+        'es2016',
+        'es2017',
+        'es2018',
+        'es2019',
+        'tc39',
+        'tc-39',
         'atom editor',
         'atomio',
         'atom.io',
         'nodejs',
         'node.js',
-        'Linus Torvalds',
-        'rust lanugage'
+        'DJI',
+        'quadcopter',
+        'AirMap',
+        'DuckDuckGo'
     ]
     blockedStrings: [
         'porn',
@@ -21,10 +38,12 @@ module.exports = {
         'immigration',
         'hate',
         'worst',
+        'don\'t like',
         'damn',
         'hell',
         'DonaldTrump',
         'PresidentTrump',
+        'Trump',
         'overrated',
         'overhyped'
     ],
