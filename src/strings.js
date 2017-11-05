ff0cd279700bfd6d585c255ac6a4cf81fcecfebc
Jade Thornton <jade@jmthornton.net>
2017-11-05 11:30:11 -0600

    remove reply strings

--- src/strings.js
+++ src/strings.js
@@ -40,15 +40,5 @@ module.exports = {
         'mixed',
         'recent',
         'popular'
-    ],
-    responseString: [
-        'Hi @screenName thanks for the follow!',
-        '@screenName thanks for following!',
-        'Hey @screenName thanks for the follow!',
-        'Thanks for following @screenName!',
-        'Hey @screenName, thanks for following!',
-        'Awesome @screenName, thanks for following!',
-        'Thanks for the follow @screenName!',
-        'Thanks for following @screenName! How are you today?'
     ]
 };
